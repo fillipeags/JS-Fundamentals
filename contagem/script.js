8c3eef112594f9a4b577daee8fdb899b64536c4d
Fillipe <fillipe484@gmail.com>
2020-09-08 23:45:25 -0300

    count

---
+++ contagem/script.js
@@ -0,0 +1,13 @@
+function count(){
+  var inicio = document.getElementById('inicio')
+
+  var submit = document.getElementById('button')
+  var res = document.getElementById('result')
+
+  var valor = parseInt(inicio)
+  res.innerHTML = `${valor}`
+  
+
+
+
+}
